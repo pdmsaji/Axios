d11600b1b3a48af9362ce7af57d644a33648a871
pdmsaji <pdmsajith@gmail.com>
2022-06-11 13:06:14 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,61 @@
+//import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import axios from 'axios';
+import { useState } from 'react'; // defined to incorporate data from the server
+
+
+
+function App() {
+ 
+   const [state,setState] = useState([]); 
+
+
+  return (
+    <div className="App">
+
+      <h1> APIs....</h1>
+
+      <button onClick={
+        ()=>{
+          axios.get('https://jsonplaceholder.typicode.com/posts').then(
+            (res)=>{
+              console.log(res.data);
+              setState(res.data);
+            }
+
+          )
+
+        }
+
+      }>
+
+        Click here to load
+      
+      </button>
+
+      {state.map(
+        (obj,index)=>{
+
+          return(
+            <div>
+
+              <h1> {index+1} </h1>
+              <h4>{obj.title}</h4>
+            </div>
+
+
+
+
+          )
+        }
+      )
+      
+      
+      }
+
+    </div>
+  );
+}
+
+export default App;
